69aa8b5f687abf4ed25c68d817e5665cbef9be4f
JesseKoldewijn <jesse.koldewijn@denieuwezaak.nl>
2024-05-16 12:06:26 +0200

    feat: small tweak on demo form

--- src/components/ImageUploadDemo.tsx
+++ src/components/ImageUploadDemo.tsx
@@ -1,19 +1,41 @@
 "use client";
 
-import { useState, type FormEvent } from "react";
+import { useState } from "react";
+import { Button } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+
+const formSchema = z.object({
+  file: z.any().optional(),
+});
 
 const ImageUploadDemo = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
+  const fileRef = form.register("file");
+
   const [fileDetails, setFileDetails] = useState<{
     message: string;
     fileName: string;
   } | null>(null);
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    const fields = e.currentTarget.elements;
-    const imageField = fields.namedItem("image") as HTMLInputElement;
-    const image = imageField.files?.item(0);
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
+    const list = data.file as FileList;
+    const image = list?.item(0);
 
     if (image) {
       const fileBase64 = await new Promise<string>((resolve) => {
@@ -41,32 +63,42 @@ const ImageUploadDemo = () => {
   };
 
   return (
-    <>
+    <div className="px-4 md:px-0">
       {fileDetails && (
         <div className="mx-auto flex max-w-md flex-col gap-2 pt-24">
           <p>{fileDetails.message}</p>
           <p>{fileDetails.fileName}</p>
         </div>
       )}
-      <form
-        onSubmit={handleSubmit}
-        className="mx-auto flex max-w-md flex-col gap-2 pt-24"
-      >
-        <input
-          name="image"
-          id="image"
-          type="file"
-          accept="image/*"
-          className="rounded-md border border-neutral-900 p-2"
-        />
-        <button
-          type="submit"
-          className="rounded-md border border-neutral-900 bg-neutral-900 p-2 text-white hover:border-neutral-300 hover:bg-neutral-300"
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="mx-auto flex max-w-md flex-col gap-2 pt-24"
         >
-          Upload
-        </button>
-      </form>
-    </>
+          <FormField
+            control={form.control}
+            name="file"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>File</FormLabel>
+                  <FormControl>
+                    <Input type="file" placeholder="shadcn" {...fileRef} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              );
+            }}
+          />
+          <Button
+            type="submit"
+            className="rounded-md border border-neutral-900 bg-neutral-900 p-2 text-white hover:border-neutral-300 hover:bg-neutral-300"
+          >
+            Upload
+          </Button>
+        </form>
+      </Form>
+    </div>
   );
 };
 export default ImageUploadDemo;
